d8758ef860417db29c1ac1dec8e11e2066be6f2e
TouficGIT <meffe.emilien@gmail.com>
2019-02-22 06:52:55 +0100

    virtual begin play

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -4,7 +4,24 @@
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 
-ATank* ATankPlayerController::GetControllerTank() const
+void ATankPlayerController::BeginPlay() 
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not posessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController posessing %s"), *(ControlledTank->GetName()));
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin play"));
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
