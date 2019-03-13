d08d3c9af058988a6aeb2a5cac90309f89950c34
TouficGIT <meffe.emilien@gmail.com>
2019-03-13 10:58:21 +0100

    projectile fire added

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 #include "Projectile.generated.h"
 
 UCLASS()
@@ -12,15 +13,16 @@ class BATTLETANK_API AProjectile : public AActor
 	GENERATED_BODY()
 	
 public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 	// Sets default values for this actor's properties
 	AProjectile();
+	void LaunchProjectile(float Speed);
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
+private:
+	UProjectileMovementComponent* ProjectileMovement = nullptr;
 };
